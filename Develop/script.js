4697211b8e78662c93baf81cea7d10672492bfcb
Alfonso Cerv <pedrocerv1@hotmail.com>
2021-04-04 15:02:57 -0500

    initial prompts created

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,81 @@
 // Assignment code here
+function passwordLength() { // function to get password length
+  var length = parseInt(window.prompt("How many characters do you want your password to be?"));
+  console.log(length)
+  if(Number.isNaN(length) ){
+  window.alert("Please enter a number!")
+  return passwordLength();
+  }
+  
+  
+  else if (length < 8) {
+    window.alert("Password must be at least 8 characters in length!")
+    return passwordLength();
+
+
+  }
+  else if (length > 128) {
+    window.alert("Password cannot be longer than 128 characters!")
+    return passwordLength();
+  }
+  else if (length >= 8 && length <= 128) {
+    return length;
+  }
+}
+
+function uppercase() { // function to get uppercase variable
+  var upperY = window.confirm("Would you like an uppercase in your password?(click cancel for 'no')")
+  if (upperY == true) {
+    return true;
+
+  }
+  else {
+    return false;
+  }
+}
+
+function lowercase() { // function to get lowercase variable
+  var lowerY = window.confirm("Would you like a lowercase in your password?(click cancel for 'no')")
+  if (lowerY == true) {
+    return true;
+
+  }
+  else {
+    return false;
+  }
+}
+
+function specialChar(){
+  var specchar = window.confirm("Would you like a lowercase in your password?(click cancel for 'no')")
+  if (specchar ==true) {
+    return true;
+
+  }
+  else {
+    return false;
+  }
+}
+
+function numeric(){
+  var numb = window.confirm("Would you like a lowercase in your password?(click cancel for 'no')")
+  if (numb == true) {
+    return true;
+
+  }
+  else {
+    return false;
+  }
+}
+
+
+function generatePassword() {
+  length = passwordLength();
+  upper = uppercase();
+  lower = lowercase();
+  number = numeric();
+  special = specialChar();
+  window.alert("Length is " + length + " Uppercase is " + upper + " Lowercase is " + lower + " numeric is " + number + " Specialchar is " + special);
+}
 
 
 // Get references to the #generate element
